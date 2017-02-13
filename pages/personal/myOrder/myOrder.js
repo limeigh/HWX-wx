97d2196dcf9baa7e1ea0f651475d6236db4e3573
李思迪 <lisidinetwork@gmail.com>
2017-02-13 17:39:37 +0800

    订单详情

--- pages/personal/myOrder/myOrder.js
+++ pages/personal/myOrder/myOrder.js
@@ -34,7 +34,6 @@ Page({
         }else if (data.data[i].type == '3') {
           data.data[i].key = that.data.recycleStatusTitles[status]?that.data.recycleStatusTitles[status]:"订单状态未知";
         }
-        // data.data[0].key = that.setupOrderStatusTitle(data.data[i]);
       }
 
       that.setData({
@@ -43,10 +42,6 @@ Page({
         totalCount:data.info.sum
       })
 
-      
-
-
-      
       console.log('lisidi'+ that.data.orderList.length);
       // console.log('cur'+ that.data.currentCount);
       var count = Number(that.orderList.length);
@@ -70,43 +65,11 @@ Page({
     var that = this;
     var index = e.currentTarget.dataset.idx;
     var order = that.data.orderList[index];
+    console.log('orderID'+ order.id);
     wx.navigateTo({
       url: '../../repair/orderDetail/orderDetail?id='+order.id,
     })
   },
 
-  setupOrderStatusTitle:function(order){
-    var status = order.status;
-    if (order.type == '1') {
-       order.statusTitle = this.data.repairStatusTitles[status]?this.data.statusTitles[status]:"订单状态未知";
-    }else if (order.type == '2') {
-      order.statusTitle = this.data.recycleStatusTitles[status]?this.data.statusTitles[status]:"订单状态未知";
-    }else {
-      order.statusTitle = '';
-    }
-    // //1.状态名称
-    // order.statusTitle = this.data.statusTitles[status]?this.data.statusTitles[status]:"订单状态未知"; 
-    // //2.状态描述
-    // var statusDesc = order.statusTitle; //默认
-    // switch(status){
-    //   case 1: statusDesc = "请注意客服来电，与您进一步确认维修信息"; break;
-    //   case 2: statusDesc = "预约上门时间为：" + util.getLocalTime(order.reserveTime); break;
-    //   case 3: statusDesc = "工程师正在上门中，工程师" + order.rName +"（工号）已出发"; break;
-    //   case 4: 
-    //   case 5: 
-    //       if(order.payStatus == 0){//未支付
-    //           statusDesc = "设备已维修完成，等待您的确认支付";
-    //       }else{ 
-    //           statusDesc = "免费上门保修日期截至" + order.WarrantyPeriod;
-    //       }
-    //       break;
-    // } 
-    // order.statusDesc = statusDesc;
-    // //3.状态图标
-    // order.statusIcon = this.data.statusIcons[status]?this.data.statusIcons[status]:"/img/order_cancelled"; //?默认图标？
-    
-    //end 更新数据
-    return order.statusTitle;
-  }
-
+ 
 })
